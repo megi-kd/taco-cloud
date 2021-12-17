6e39d4f7f8ac23daeb15f5faa8364cefa697e737
Megi <mkceva@gmail.com>
2021-12-17 23:05:34 +0100

    Ch 3.1: order and ingredient jdbc repository

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.data.OrderRepository;
 import tacos.domain.TacoOrder;
 
 import javax.validation.Valid;
@@ -14,7 +14,15 @@ import javax.validation.Valid;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepository;
+
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("current")
     public String orderForm(Model model){
@@ -23,10 +31,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors){
+    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors,
+                               SessionStatus sessionStatus){
         if(errors.hasErrors()){
             return "orderForm";
         }
+        orderRepository.save(tacoOrder);
+        sessionStatus.setComplete();
         log.info("Order submitted :" + tacoOrder);
         return "redirect:/";
     }
