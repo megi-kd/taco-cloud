204dcfca4414811cf59df7dd00ebb9f60330f105
Magdalena <mkceva@gmail.com>
2022-03-24 23:15:07 +0100

    populating a database with @Bean ApplicationRunner

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,7 +1,11 @@
 package tacos;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tacos.data.IngredientRepository;
+import tacos.domain.Ingredient;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +14,10 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner dataLoader(IngredientRepository ingredientRepository){
+		return args -> {
+			ingredientRepository.save(new Ingredient("FLT2", "Flour Tortilla 2", Ingredient.Type.WRAP));
+		};
+	}
 }
